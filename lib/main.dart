1ce01a0e4af4fb5385ea7822e3e497d3b0d5d66c
xilin <1026771640@qq.com>
2019-05-09 16:26:37 +0800

    flutter地址修改

--- lib/main.dart
+++ lib/main.dart
@@ -6,12 +6,33 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData(primarySwatch: Colors.red),
       title: ' Welcome tohich',
       home: Scaffold(
         appBar: AppBar(
+          centerTitle: true,
           title: Text('Welcome  - Gowhich'),
+          actions: <Widget>[
+            IconButton(
+              icon: Icon(Icons.shopping_cart),
+              tooltip: 'Open g cart',
+              onPressed: () {
+                print('Shopping cart opened.');
+              },
+            )
+          ],
         ),
-        body: Text("data"),
+        body: Row(children: <Widget>[
+          Expanded(child: Text('主体内容1'), flex: 2,),
+          Expanded(child: Text('主体内容2'), flex: 1,)
+        ],
+        mainAxisAlignment: MainAxisAlignment.spaceBetween),
+        floatingActionButton: FloatingActionButton(
+          tooltip: "hello world",
+          onPressed: () {},
+          child: Icon(Icons.add),
+    ),
+        drawer: Drawer(),
       ),
     );
   }
